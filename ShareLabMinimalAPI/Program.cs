4efd8076938e0aed063902e5ddcc704c282095e7
EsneiderSM <esneider.marulanda@gmail.com>
2024-04-17 21:50:40 -0500

    Class program

--- ShareLabMinimalAPI/Program.cs
+++ ShareLabMinimalAPI/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+var appName = builder.Configuration.GetValue<string>("AppName");
+
+// Start services area
+
+
+// End services area
+
+
 var app = builder.Build();
 
+// Start middlewarte area
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/appName", () => appName);
+
+// End middleware area
 
 app.Run();
